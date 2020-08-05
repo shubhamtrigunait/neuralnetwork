4fc63a8d9024a2775e6b0c0b6288e5610d04de9a
Kunal Arneja <kunal.arneja@narvar.com>
2020-08-05 17:25:19 +0530

    basic testing

--- src/main/java/com/kunal/neuralnetwork/NeuralnetworkApplication.java
+++ src/main/java/com/kunal/neuralnetwork/NeuralnetworkApplication.java
@@ -1,6 +1,6 @@
 package com.kunal.neuralnetwork;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.wnameless.json.flattener.JsonFlattener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.util.StringUtils;
@@ -10,9 +10,11 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 
 @SpringBootApplication
 public class NeuralnetworkApplication {
@@ -20,24 +22,29 @@ public class NeuralnetworkApplication {
     public static void main(String[] args) {
         SpringApplication.run(NeuralnetworkApplication.class, args);
         List<Map<String, Object>> orders = readFile();
-        List<String> l = new ArrayList<>(orders.get(0).keySet());
-        NeuralNetwork brain = new NeuralNetwork(25, 10, 3);
+        Set<String> keys = new HashSet<>();
+        for (int i = 0; i < orders.size(); i++) {
+            keys.addAll(orders.get(i).keySet());
+        }
+        List<String> l = new ArrayList<>(keys);
+        int numInputNodes = l.size();
+        NeuralNetwork brain = new NeuralNetwork(numInputNodes, 10, 3);
         brain.setLearningRate(0.05);
         brain.setActivationFunction(new NeuralNetwork.ActivationFunction(Mat.TANH, Mat.TANH_DERIVATIVE));
-        List<Integer> indexes = new ArrayList<>();
+        Set<Integer> indexes = new HashSet<>();
 
         for (int iter = 0; iter < 100; iter++) {
             for (int i = 0; i < orders.size(); i++) {
-                double[] inputs = new double[25];
+                double[] inputs = new double[numInputNodes];
                 double[] outputs = new double[3];
-                for (int j = 0; j < 25; j++) {
+                for (int j = 0; j < numInputNodes; j++) {
                     if (Objects.nonNull(orders.get(i).get(l.get(j)))) {
                         inputs[j] = Double.valueOf(orders.get(i).get(l.get(j)).hashCode()) / Integer.MAX_VALUE;
                     }
                 }
                 if (!StringUtils.isEmpty(orders.get(i).get("fulfillment_status")) &&
                         orders.get(i).get("fulfillment_status").toString().equalsIgnoreCase("SHIPPED")) {
-                    if (Objects.nonNull(orders.get(i).get("shipment_info")) && ((List) orders.get(i).get("shipment_info")).size() > 0) {
+                    if (!StringUtils.isEmpty(orders.get(i).get("shipment_info[0].tracking_number"))) {
                         outputs[0] = 1;
                         outputs[1] = 0;
                         outputs[2] = 0;
@@ -57,42 +64,38 @@ public class NeuralnetworkApplication {
             }
         }
 
-
+        List<Integer> indexesList = new ArrayList<>(indexes);
         for (int k = 0; k < indexes.size(); k++) {
-            int i = indexes.get(k);
+            int i = indexesList.get(k);
             if (!StringUtils.isEmpty(orders.get(i).get("fulfillment_status")) &&
                     orders.get(i).get("fulfillment_status").toString().equalsIgnoreCase("SHIPPED")) {
                 if (Objects.nonNull(orders.get(i).get("shipment_info")) && ((List) orders.get(i).get("shipment_info")).size() > 0) {
                     System.out.println("1 0 0");
-                }
-                else{
+                } else {
                     System.out.println("0 0 1");
                 }
 
             } else {
                 System.out.println("0 1 0");
             }
-            double[] inputs = new double[25];
-            for (int j = 0; j < 25; j++) {
+            double[] inputs = new double[numInputNodes];
+            for (int j = 0; j < numInputNodes; j++) {
                 if (Objects.nonNull(orders.get(i).get(l.get(j)))) {
                     inputs[j] = Double.valueOf(orders.get(i).get(l.get(j)).hashCode()) / Integer.MAX_VALUE;
                 }
             }
             double[] outputs = brain.process(inputs);
-            System.out.println("Test: " + Arrays.toString(inputs));
-            System.out.println(" -> " + Arrays.toString(outputs));
+            System.out.println("OUTPUT -> " + Arrays.toString(outputs));
         }
     }
 
     public static List<Map<String, Object>> readFile() {
-        String line = "";
+        String line;
         List<Map<String, Object>> orders = new ArrayList<>();
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            String file = "src/main/java/com/kunal/neuralnetwork/input.txt";
-            BufferedReader br = new BufferedReader(new FileReader(file));
+        String file = "src/main/java/com/kunal/neuralnetwork/input.txt";
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             while ((line = br.readLine()) != null) {
-                orders.add(mapper.readValue(line, Map.class));
+                orders.add(JsonFlattener.flattenAsMap(line));
             }
         } catch (IOException e) {
             e.printStackTrace();
