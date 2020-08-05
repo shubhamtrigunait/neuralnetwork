2ab18aad5328b242b243c5946ceab7cb67f55ea7
Kunal Arneja <kunal.arneja@narvar.com>
2020-08-05 15:12:29 +0530

    test neural network

--- src/main/java/com/kunal/neuralnetwork/NeuralnetworkApplication.java
+++ src/main/java/com/kunal/neuralnetwork/NeuralnetworkApplication.java
@@ -3,6 +3,7 @@ package com.kunal.neuralnetwork;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StringUtils;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -11,6 +12,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @SpringBootApplication
 public class NeuralnetworkApplication {
@@ -19,23 +21,50 @@ public class NeuralnetworkApplication {
         SpringApplication.run(NeuralnetworkApplication.class, args);
         List<Map<String, Object>> orders = readFile();
         List<String> l = new ArrayList<>(orders.get(0).keySet());
-        System.out.println(l);
-        NeuralNetwork brain = new NeuralNetwork(2, 2, 2);
+        NeuralNetwork brain = new NeuralNetwork(25, 10, 2);
         brain.setLearningRate(0.05);
         brain.setActivationFunction(new NeuralNetwork.ActivationFunction(Mat.TANH, Mat.TANH_DERIVATIVE));
+        List<Integer> indexes = new ArrayList<>();
 
-        int[][] xorInputs = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
-        for (int i = 0; i < xorInputs.length * 2000; i++) {
-            int[] arr = xorInputs[i % 4];
-            double[] inputs = {arr[0], arr[1]};
-            double[] outputs = {arr[0] ^ arr[1], arr[0] ^ arr[1]};
+        for (int i = 0; i < orders.size(); i++) {
+            double[] inputs = new double[25];
+            double[] outputs = new double[2];
+            for (int j = 0; j < 25; j++) {
+                if (Objects.nonNull(orders.get(i).get(l.get(j)))) {
+                    inputs[j] = Double.valueOf(orders.get(i).get(l.get(j)).hashCode())/ Integer.MAX_VALUE;
+                }
+            }
+            if (!StringUtils.isEmpty(orders.get(i).get("fulfillment_status")) &&
+                    orders.get(i).get("fulfillment_status").toString().equalsIgnoreCase("SHIPPED")) {
+                outputs[0] = 1;
+                outputs[1] = 0;
+                indexes.add(i);
+                indexes.add(i + 1);
+            } else {
+                outputs[0] = 0;
+                outputs[1] = 1;
+            }
             brain.train(inputs, outputs);
         }
 
-        for (int i = 0; i < xorInputs.length; i++) {
-            double[] inputs = {xorInputs[i][0], xorInputs[i][1]};
+
+        for (int k = 0; k < indexes.size(); k++) {
+            int i = indexes.get(k);
+            if (!StringUtils.isEmpty(orders.get(i).get("fulfillment_status")) &&
+                    orders.get(i).get("fulfillment_status").toString().equalsIgnoreCase("SHIPPED")) {
+                System.out.println("SHIPPEDDDDDD");
+            } else {
+                System.out.println("NOT SHIPPEDDDDDD");
+            }
+            double[] inputs = new double[25];
+            for (int j = 0; j < 25; j++) {
+                if (Objects.nonNull(orders.get(i).get(l.get(j)))) {
+                    inputs[j] = Double.valueOf(orders.get(i).get(l.get(j)).hashCode()) / Integer.MAX_VALUE;
+                }
+            }
             double[] outputs = brain.process(inputs);
-            System.out.println("Test: " + Arrays.toString(inputs) + " -> " + Arrays.toString(outputs));
+            System.out.println("Test: " + Arrays.toString(inputs));
+            System.out.println(" -> " + Arrays.toString(outputs));
         }
     }
 
