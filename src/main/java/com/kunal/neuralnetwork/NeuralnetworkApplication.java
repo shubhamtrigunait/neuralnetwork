71a40ede858831c3e78a8e001a97b621571c0b77
Kunal Arneja <kunal.arneja@narvar.com>
2020-08-05 16:38:38 +0530

    test train

--- src/main/java/com/kunal/neuralnetwork/NeuralnetworkApplication.java
+++ src/main/java/com/kunal/neuralnetwork/NeuralnetworkApplication.java
@@ -21,30 +21,40 @@ public class NeuralnetworkApplication {
         SpringApplication.run(NeuralnetworkApplication.class, args);
         List<Map<String, Object>> orders = readFile();
         List<String> l = new ArrayList<>(orders.get(0).keySet());
-        NeuralNetwork brain = new NeuralNetwork(25, 10, 2);
+        NeuralNetwork brain = new NeuralNetwork(25, 10, 3);
         brain.setLearningRate(0.05);
         brain.setActivationFunction(new NeuralNetwork.ActivationFunction(Mat.TANH, Mat.TANH_DERIVATIVE));
         List<Integer> indexes = new ArrayList<>();
 
-        for (int i = 0; i < orders.size(); i++) {
-            double[] inputs = new double[25];
-            double[] outputs = new double[2];
-            for (int j = 0; j < 25; j++) {
-                if (Objects.nonNull(orders.get(i).get(l.get(j)))) {
-                    inputs[j] = Double.valueOf(orders.get(i).get(l.get(j)).hashCode())/ Integer.MAX_VALUE;
+        for (int iter = 0; iter < 100; iter++) {
+            for (int i = 0; i < orders.size(); i++) {
+                double[] inputs = new double[25];
+                double[] outputs = new double[3];
+                for (int j = 0; j < 25; j++) {
+                    if (Objects.nonNull(orders.get(i).get(l.get(j)))) {
+                        inputs[j] = Double.valueOf(orders.get(i).get(l.get(j)).hashCode()) / Integer.MAX_VALUE;
+                    }
                 }
+                if (!StringUtils.isEmpty(orders.get(i).get("fulfillment_status")) &&
+                        orders.get(i).get("fulfillment_status").toString().equalsIgnoreCase("SHIPPED")) {
+                    if (Objects.nonNull(orders.get(i).get("shipment_info")) && ((List) orders.get(i).get("shipment_info")).size() > 0) {
+                        outputs[0] = 1;
+                        outputs[1] = 0;
+                        outputs[2] = 0;
+                    } else {
+                        outputs[0] = 0;
+                        outputs[1] = 0;
+                        outputs[2] = 1;
+                    }
+                    indexes.add(i);
+                    indexes.add(i + 1);
+                } else {
+                    outputs[0] = 0;
+                    outputs[1] = 1;
+                    outputs[2] = 0;
+                }
+                brain.train(inputs, outputs);
             }
-            if (!StringUtils.isEmpty(orders.get(i).get("fulfillment_status")) &&
-                    orders.get(i).get("fulfillment_status").toString().equalsIgnoreCase("SHIPPED")) {
-                outputs[0] = 1;
-                outputs[1] = 0;
-                indexes.add(i);
-                indexes.add(i + 1);
-            } else {
-                outputs[0] = 0;
-                outputs[1] = 1;
-            }
-            brain.train(inputs, outputs);
         }
 
 
@@ -52,9 +62,15 @@ public class NeuralnetworkApplication {
             int i = indexes.get(k);
             if (!StringUtils.isEmpty(orders.get(i).get("fulfillment_status")) &&
                     orders.get(i).get("fulfillment_status").toString().equalsIgnoreCase("SHIPPED")) {
-                System.out.println("SHIPPEDDDDDD");
+                if (Objects.nonNull(orders.get(i).get("shipment_info")) && ((List) orders.get(i).get("shipment_info")).size() > 0) {
+                    System.out.println("1 0 0");
+                }
+                else{
+                    System.out.println("0 0 1");
+                }
+
             } else {
-                System.out.println("NOT SHIPPEDDDDDD");
+                System.out.println("0 1 0");
             }
             double[] inputs = new double[25];
             for (int j = 0; j < 25; j++) {
